254cabcc9805b283fb3d0b460b6a9e0682f74911
Artherzno <zkyzno@Artherznos-MacBook-Pro.local>
2020-11-16 16:24:01 +0700

    - Add scss

--- src/App.js
+++ src/App.js
@@ -1,129 +1,13 @@
-import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react'
 
-import CountPage from './pages/Countpage';
-import ShowParam from './pages/ShowParam';
+import './App.scss'
 
-export default function BasicExample() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/showparam/test">ShowParam</Link>
-          </li>
-          <li>
-            <Link to="/countpage">CountPage</Link>
-          </li>
-          <li>
-            <Link to="/mycomponent">MyComponent</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/showparam/:data">
-            <ShowParam />
-          </Route>
-          <Route path="/countpage">
-            <CountPage />
-          </Route>
-          <Route path="/mycomponent">
-            <MyComponent />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-// You can think of these components as "pages"
-// in your app.
-
-function Home() {
+function App() {
   return (
     <div>
-      <h2>Home</h2>
+      Test
     </div>
-  );
+  )
 }
 
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result);
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>
-              {item.name}, id: {item.id}
-            </li>
-          ))}
-        </ul>
-      );
-    }
-  }
-}
+export default App
