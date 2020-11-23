c2005f02965d629a554ec695a878fe01b7116723
artherzno <artherzno@gmail.com>
2020-11-23 11:04:08 +0700

    - Change name transaction item

--- src/components/MainTransaction.js
+++ src/components/MainTransaction.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import MainTransactionItem from './MainTransactionItem';
 
 function MainTransaction() {
     return (
         <div className="main-transaction">
             <h3>ประวัติรายการ</h3>
-            
+            <MainTransactionItem />
         </div>
     )
 }
---
+++ src/components/MainTransactionItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MainTransactionItem() {
+    return (
+        <div className="main-transaction-item ">
+            <img />
+            <div>
+                <h3>ซื้อเหรียญเพิ่ม</h3>
+                <p>detail</p>
+            </div>
+        </div>
+    )
+}
+
+export default MainTransactionItem;
