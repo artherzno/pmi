fce3ec45163666459ce87f9db764b792393538e4
artherzno <artherzno@gmail.com>
2020-12-01 10:37:10 +0700

    - Edit buycoin action

--- src/components/BuyCoinAction.js
+++ src/components/BuyCoinAction.js
@@ -3,28 +3,41 @@ import { Link } from 'react-router-dom';
 
 import BtnEllipse from './BtnEllipse';
 
+import IconVIP from '../assets/img/ic-vip.svg'
+import IconStar from '../assets/img/ic-star.svg'
+import IconPlus from '../assets/img/ic-plus.svg'
+import IconMinus from '../assets/img/ic-minus.svg'
+
 function BuyCoinAction() {
     return (
         <div className="buycoin-action">
             <div className="buycoin-action-box box-style">
-                <div>
-                    <img/>
+                <div className="_header">
+                    <img src={IconVIP} alt="icon" />
                     <span>คะแนน VIP ที่ใช้</span>
                 </div>
-                <input type="number" step="1000" min="1000" max="3000" />
-                <p className="buycoin-error">คุณมีคะแนนไม่เพียงพอ</p>
-                <div className="">
+                <div className="_input">
+                    <img src={IconMinus} alt="icon" />
+                    <input type="number" step="1000" min="1000" max="3000" />
+                    <img src={IconPlus} alt="icon" />
+                    <p className="buycoin-error">คุณมีคะแนนไม่เพียงพอ</p>
+                </div>
+                <div className="_badge">
                     <p>1 คะแนน แลกเหรียญได้ 1 เหรียญ</p>
-                    <div>
-                        <div>1,000 คะแนน</div>
-                        <div>2,000 คะแนน</div>
-                        <div>3,000 คะแนน</div>
+                    <div className="_badgelist">
+                        <div className="_badgeitem">1,000 คะแนน</div>
+                        <div className="_badgeitem">2,000 คะแนน</div>
+                        <div className="_badgeitem">3,000 คะแนน</div>
                     </div>
                 </div>
-                <div className="buycoin-getcoin">
-                    <span>ได้รับเหรียญ</span>
-                    <img/>
-                    <span>50 เหรียญ</span>
+                <div className="_footer">
+                    <div className="_left">
+                        <span>ได้รับเหรียญ</span>
+                    </div>
+                    <div className="_right">
+                        <img src={IconStar} alt="icon" />
+                        <span>50 เหรียญ</span>
+                    </div>
                 </div>
             </div>
             <Link to="/">
